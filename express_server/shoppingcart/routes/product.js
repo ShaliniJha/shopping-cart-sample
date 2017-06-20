c3d0a5db1a5cfb3252029d42a6dca67a451752db
Shalini Jha <sjha11@sapient.com>
2017-06-20 20:25:12 +0530

    Added express server

---
+++ express_server/shoppingcart/routes/product.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = requier('mongoose');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+});
+
+module.exports = router;
